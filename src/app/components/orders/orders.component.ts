22cc2f74b8f531b758e47ab6689241915ada1db8
fady bouez <59925759+fadybouez@users.noreply.github.com>
2023-03-07 20:53:38 +0200

    create orders component and page ui

---
+++ src/app/components/orders/orders.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ServiceService } from 'src/app/services/service.service';
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.scss']
+})
+export class OrdersComponent implements OnInit {
+
+  constructor(private serviceService:ServiceService) { }
+orders:any
+//get orders from service
+
+  ngOnInit(): void {
+this.serviceService.getOrders().subscribe((data:any)=>{
+  this.orders=data
+},(error:any)=>{console.log(error)})
+  }
+
+}
